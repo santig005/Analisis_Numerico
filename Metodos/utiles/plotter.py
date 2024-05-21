18d9e0931f33a6d69918416d1bc5fec5062e67fe
santig005 <sajhdg30@gmail.com>
2024-05-21 18:04:02 -0500

    funciones para guardar texto y graficar una funcion sin puntos

--- Metodos/utiles/plotter.py
+++ Metodos/utiles/plotter.py
@@ -19,3 +19,18 @@ def plot_fx_puntos(funcion,xinicial,xfinal,puntosx,puntosy,alias_punto):
             xaxis_title='x',
             yaxis_title='f(x)')
     return fig
+
+def fx_plot(funcion,xinicial,xfinal):
+    x = sp.Symbol('x')
+    funcion_expr = parse_expr(funcion, local_dict={'x': x})
+    intervalo_x = np.arange(xinicial, xfinal,0.1)
+    fx = sp.lambdify(x, funcion_expr, 'numpy')
+    intervalo_y = fx(intervalo_x)
+    intervalo_x_completo = np.arange(xinicial, xfinal,0.1)
+    intervalo_y_completo = fx(intervalo_x_completo)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=intervalo_x_completo, y=intervalo_y_completo, mode='lines', name='f(x)'))
+    fig.update_layout(title=f'Función: {funcion} en intervalo [{xinicial}, {xfinal}]',
+            xaxis_title='x',
+            yaxis_title='f(x)')
+    return fig
